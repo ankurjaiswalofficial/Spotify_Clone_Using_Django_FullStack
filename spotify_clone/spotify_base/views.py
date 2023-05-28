d5a640366255ced3259cfc07bb7719fcc0842cc1
Ankur Jaiswal <117668534+ankurjaiswalofficial@users.noreply.github.com>
2023-05-28 19:00:37 +0530

    Basic Setup And Frontend with Backend Linkage

--- spotify_clone/spotify_base/views.py
+++ spotify_clone/spotify_base/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+default_context = {
+    "title": "Spotify Clone - Ankur Jaiswal"
+}
+
+
+def home_view(request):
+    return render(request, template_name="base.html", context=default_context)
--- spotify_clone/spotify_clone/settings.py
+++ spotify_clone/spotify_clone/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "statics/"
+# STATIC_ROOT = BASE_DIR / "statics/"
 STATICFILES_DIRS = [
     BASE_DIR / "statics",
 ]
